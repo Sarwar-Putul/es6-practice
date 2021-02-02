3906ab717a7a53a3d8cc1c47055abca437be5438
Sarwar-Putul <sarwarputul@gmail.com>
2021-02-02 19:27:23 +0600

    all ok

--- arrow.js
+++ arrow.js
@@ -1,33 +1,33 @@
-// function doubleIt(num) {
-//     return num * 2;
-// }
-// const result = doubleIt(5);
-// console.log(result); // স্বাভাবিক ফাংশন।
+function doubleIt(num) {
+    return num * 2;
+}
+const result = doubleIt(5);
+console.log(result); // স্বাভাবিক ফাংশন।
 
-// // অথবা
-// const doubleIt = function(num) {
-//     return num * 2;
-// }
-// const result = doubleIt(5);
-// console.log(result);
+// অথবা
+const doubleIt = function(num) {
+    return num * 2;
+}
+const result = doubleIt(5);
+console.log(result);
 
-// // অথবা
-// const doubleIt = num => num * 2;
-// const result = doubleIt(5);
-// console.log(result); //(=>) arrow ফাংশন দিয়ে লিখলে (=>) চিহ্নের পূর্বে থাকে প্যারামিটার এবং পরে থাকে return ।
+// অথবা
+const doubleIt = num => num * 2;
+const result = doubleIt(5);
+console.log(result); //(=>) arrow ফাংশন দিয়ে লিখলে (=>) চিহ্নের পূর্বে থাকে প্যারামিটার এবং পরে থাকে return ।
 
-// // অথবা
-// const add = (x, y) => x + y;
-// const result = add(50, 70);
-// console.log(result); // (=>) arrow ফাংশনে দুইটা প্যারামিটার থাকলে ১ম বন্ধনি ()  দিয়ে লিখতে হয়।
+// অথবা
+const add = (x, y) => x + y;
+const result = add(50, 70);
+console.log(result); // (=>) arrow ফাংশনে দুইটা প্যারামিটার থাকলে ১ম বন্ধনি ()  দিয়ে লিখতে হয়।
 
-// // অথবা
+// অথবা
 
-// const give5 = () => 5;
-// const result = give5();
-// console.log(result); // (=>) arrow ফাংশনে কুনো প্যারামিটার না থাকলে একটা ফাকা বন্ধনি দিতে হয়। 
+const give5 = () => 5;
+const result = give5();
+console.log(result); // (=>) arrow ফাংশনে কুনো প্যারামিটার না থাকলে একটা ফাকা বন্ধনি দিতে হয়। 
 
-// // অথবা
+// অথবা
 
 const doMath = (x, y) => {
     const sum = x + y;

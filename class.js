fefc799ff5c8414cc13ee29a642a0e79b46779fd
Sarwar-Putul <sarwarputul@gmail.com>
2021-02-02 21:09:31 +0600

    Class done

---
+++ class.js
@@ -0,0 +1,12 @@
+class Student {
+    constructor(sId, sName) {
+        this.id = sId;
+        this.name = sName;
+        this.school = "Bangladesh Film Industries";
+    }
+}
+const student1 = new Student(25, "Shuvo");
+const student2 = new Student(21, "Mahiya");
+const student3 = new Student(25, "Ananta");
+console.log(student1, student2, student3);
+//console.log(student1.name, student2.name, student3.name);
